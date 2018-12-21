c48ff70f67eed079878b460ee40a597de9d3779c
zhangrs <vmfuyun@163.com>
2018-12-21 13:58:07 +0800

    2018-12-21 脚本编写完成，待验证

--- cobbler-install.sh
+++ cobbler-install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash   
+#基于CentOS 7 部署cobbler服务
+#新建VM的IP为自动获取可上网
+
 yum install epel-release -y 
 yum clean all 
 
-yum install cobbler cobbler-web httpd rsync tftp-server  xinetd dhcp python-ctypes  debmirror  pykickstart  -y 
+yum install cobbler cobbler-web httpd rsync tftp-server  xinetd dhcp python-ctypes  debmirror  pykickstart  fence-agents-all -y 
+
 
 systemctl enable cobblerd  
 systemctl  start cobblerd  
@@ -52,25 +56,86 @@ service network restart
 
 #配置DHCP  
 
-read -t 30 -p "请输入IP地址段：" subnet
-read -t 30 -p "请输入IP起始地址：" HeadIP
-read -t 30 -p "请输入IP结束地址：" EndIP
+read -t 30 -p "请输入DHCP可分配地址段subnet：" subnet
+read -t 30 -p "请输入range for HeadIP：" HeadIP
+read -t 30 -p "请输入range for EndIP：" EndIP
 
 read -t 30 -p "请输入routers:" ROUTER
 
+cp /etc/dhcp/dhcpd.conf  /etc/dhcp/dhcpd.conf.bak
 
 echo -e "subnet $subnet  netmask $NETMASK{\n range $HeadIP $EndIP;\n option domain-name-servers ns1.internal.example.org;\n option domain-name "internal.example.org";\n option routers $ROUTER;\n option broadcast-address 192.168.0.255;\n default-lease-time 600;\n max-lease-time 7200;\n next-server $IP;\n filename "pxelinux.0";\n}\n"   >> /etc/dhcp/dhcpd.conf
 
 
-systemctl start dhcpd 
+systemctl restart dhcpd 
 systemctl enable dhcpd 
  
  
 #配置cobbler 
 
+#备份源文件 
+cp /etc/cobbler/setting /etc/cobbler/setting.bak
+
+
 #修改setting  server and next_server 
 sed -i 's/^server:127.0.0.1/server:$IP'
 sed -i 's/^next_server:127.0.1/next_server:$IP/g' /etc/cobbler/setting 
+
+
+#设置tftp服务被xinetd托管 
+
+cp /etc/xinetd.d/tftp  /etc/xinetd.d/tftp.bak
+
+sed -i /disable/s/yes/no/ /etc/xinetd.d/tftp
+
+#restart tftp  
+
+systemctl restart rsyncd.service
+systemctl enable rsyncd.service
+
+#安装网络引导文件boot-loaders
+
+cobbler get-loaders 
+
+#openssl passwd -1 -salt  Gues OS now passwd
+#bak 
+cp /etc/cobbler/setting /etc/cobbler/setting.openssl.bak
+
+echo "Backup before password change">>/etc/cobbler/setting.openssl.bak
+
+read -t 30 -p "请输入guest OS 密码：" now
+
+echo "guest OS password  $now"
+
+n=$(openssl passwd -1 -salt 'password' "$now")
+
+echo now passwd
+echo $n
+
+o=$(grep ^default_password_crypted:/etc/cobbler/setting)
+
+echo old passwd
+echo $o
+
+
+sed -i "s#$o#default_password_crypted:\"$n\"#g" /etc/cobbler/setting
+
+#restart service 
+cobbler sync
+systemctl restart cobblerd 
+systemctl enable cobblerd
+systemctl enable rsyncd
+systemctl restart rsyncd
+systemctl restart dhcpd
+systemctl enable  dhcpd
+systemctl restart httpd
+systemctl enable httpd  
+
+cobbler check
+
+
+
+
 
 
 
